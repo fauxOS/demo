7ea1cde6a94524b7cb575778f6e1661402da57c2
Daniel E <danielethridge@icloud.com>
2017-05-08 10:47:21 -0400

    Create input.vue

---
+++ src/components/input.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+    <input v-model="input">
+	</div>
+</template>
+
+<script>
+export default {
+  name: "input",
+  data() {
+    return {
+      input: ""
+    }
+  }
+}
+</script>
+
+<style>
+input {
+  font-size: 15px;
+  font-family: dejavu sans mono, monospace;
+}
+</style>
