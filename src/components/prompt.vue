58e16627de4112b4ca961f73430ebcb8571a71ad
Daniel E <danielethridge@icloud.com>
2017-05-08 10:53:28 -0400

    Create prompt.vue

---
+++ src/components/prompt.vue
@@ -0,0 +1,22 @@
+<template>
+  <span class="prompt">{{ prompt }}</span>
+</template>
+
+<script>
+export default {
+  name: "prompt",
+  data() {
+    return {
+      prompt: "faux:/#"
+    }
+  }
+}
+</script>
+
+<style>
+.prompt {
+  font-size: 15px;
+  font-family: dejavu sans mono, monospace;
+  color: #808080
+}
+</style>
