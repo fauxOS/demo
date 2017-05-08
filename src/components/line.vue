3d57f7d6d913e0ea482bcb4cb969dbf9b27075a1
Daniel E <danielethridge@icloud.com>
2017-05-08 10:38:25 -0400

    Create line.vue

---
+++ src/components/line.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <console-prompt></console-prompt>
+    <console-input></console-input>
+  </div>
+</template>
+
+<script>
+import ConsolePrompt from "./prompt.vue";
+import ConsoleInput from "./input.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {}
+  },
+  components: {
+    ConsolePrompt,
+    ConsoleInput
+  }
+}
+</script>
+
+<style>
+#app {
+  font-size: 15px;
+  font-family: dejavu sans mono, monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: rgb(48, 57, 66);
+}
+
+a {
+  color: #42b983;
+}
+</style>
