aa1a5082ea634b7b9be3f71e42d05b4222680ca0
Daniel Ethridge <danielethridge@icloud.com>
2017-05-07 21:21:52 -0400

    Vue webpack-simple boilerplate project

---
+++ src/components/main.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Faux OS Demo"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
